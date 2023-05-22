3c5bf482d0aae0ea1245c785f7453b26ffda32e8
minkiko@gmail.com <minkiko@gmail.com>
2023-05-22 17:05:36 -0500

    Revised javascript to show correct date and time on header

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+// $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +20,72 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+  // });
+
+
+// Global variable
+var today = moment();
+// referencing the entire whole scheduler
+var timeTask = document.querySelector('.container');
+
+
+// Displays the current date and time in the header
+$('#currentDay').text(today.format('LLLL')); // LLLL shows day of the week, date, and time
+
+// Event listener for saveButton
+$('.saveButton').on('click', function () {
+  // get nearby values of the description in jQuery
+  var textValue = $(this).siblings('.description').val();
+  // get the id attribute of the parent element
+  var timeValue = $(this).parent().attr('id');
+
+  // saves value in local storage
+  localStorage.setItem(timeValue, textValue);
+});
+
+// Get item from local storage. Referencing the ID of hours and class description of HTML
+$('#hour8 .description').val(localStorage.getItem('hour8'));
+$('#hour9 .description').val(localStorage.getItem('hour9'));
+$('#hour10 .description').val(localStorage.getItem('hour10'));
+$('#hour11 .description').val(localStorage.getItem('hour11'));
+$('#hour12 .description').val(localStorage.getItem('hour12'));
+$('#hour13 .description').val(localStorage.getItem('hour13'));
+$('#hour14 .description').val(localStorage.getItem('hour14'));
+$('#hour15 .description').val(localStorage.getItem('hour15'));
+$('#hour16 .description').val(localStorage.getItem('hour16'));
+$('#hour17 .description').val(localStorage.getItem('hour17'));
+
+// Function to track tasks and make them change colors if they are in the past, present or future
+function auditTask() {
+  // get current number of hours
+  var currentHour = today.hours();
+
+  // loop over each time block
+  $('.time-block').each(function () {
+    var timeBlock = parseInt($(this).attr('id').split("hour")[1]);
+
+    // if the time Id attribute is before the current hour, add the past class
+    if (timeBlock < currentHour) {
+      $(this).addClass('past');
+    } // if the time Id attribute is equal to the current hour, remove the past and future classes and add the current class
+    else if (timeBlock === currentHour) {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    } // If the time Id attribute is greater than the current time, remove the past and current classes and add the future class
+    else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
   });
+}
+
+// call to run function auditTask on line 59
+auditTask();
+
+// Use setTimeout function to update the website's timer every minute (60,000 ms on line 91)
+setTimeout(function () {
+  // clears the current URL
+  location = ''; // location references the current URL
+}, 60000);
